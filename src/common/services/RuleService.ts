88ce6f3ae87bba63ef9430d4be9d38f652e3e229
Joachim Dalen <me@d3v.no>
2021-12-19 08:28:20 +0100

    refactor(core): Break down rule service updateRule

--- src/common/services/RuleService.ts
+++ src/common/services/RuleService.ts
@@ -1,3 +1,4 @@
+import { IProjectInfo } from 'azure-devops-extension-api';
 import { v4 as uuidV4 } from 'uuid';
 
 import ProjectConfigurationDocument from '../models/ProjectConfigurationDocument';
@@ -47,41 +48,44 @@ class RuleService {
       };
     }
     if (this._data === undefined) {
-      const newDocument: ProjectConfigurationDocument = {
-        id: project.id,
-        workItemRules: [{ workItemType: rule.workItemType, rules: [{ ...rule, id: uuidV4() }] }]
-      };
-      const created = await this._dataStore.setData(newDocument);
-      this._data = created;
-      return {
-        success: true,
-        data: created
-      };
+      const createResult = await this.createRootDocument(project, rule);
+      return createResult;
     }
 
     const wiIndex = this._data.workItemRules?.findIndex(x => x.workItemType === workItemType);
     if (wiIndex !== undefined && wiIndex >= 0) {
-      const ruleDocument = this._data.workItemRules[wiIndex];
-      const ruleIndex = ruleDocument.rules.findIndex(x => x.id === rule?.id);
-      if (ruleIndex >= 0) {
-        ruleDocument.rules[ruleIndex] = rule;
-      } else {
-        ruleDocument.rules = [...ruleDocument.rules, rule];
-      }
-      const nd = { ...this._data };
-      nd.workItemRules[wiIndex] = ruleDocument;
-      const updatedDocument = await this._dataStore.setData(nd);
-
-      this._data = updatedDocument;
-      return {
-        success: true,
-        data: updatedDocument
-      };
+      const uResult = await this.updateOrAddRule(this._data, wiIndex, rule);
+      return uResult;
     }
+
+    const wiResult = await this.createWorkItemTypeDocument(this._data, rule);
+    return wiResult;
+  }
+
+  private async createRootDocument(
+    project: IProjectInfo,
+    rule: Rule
+  ): Promise<ActionResult<ProjectConfigurationDocument>> {
+    const newDocument: ProjectConfigurationDocument = {
+      id: project.id,
+      workItemRules: [{ workItemType: rule.workItemType, rules: [{ ...rule, id: uuidV4() }] }]
+    };
+    const created = await this._dataStore.setData(newDocument);
+    this._data = created;
+    return {
+      success: true,
+      data: created
+    };
+  }
+
+  private async createWorkItemTypeDocument(
+    rootDoc: ProjectConfigurationDocument,
+    rule: Rule
+  ): Promise<ActionResult<ProjectConfigurationDocument>> {
     const newDoc: ProjectConfigurationDocument = {
-      ...this._data,
+      ...rootDoc,
       workItemRules: [
-        ...this._data.workItemRules,
+        ...rootDoc.workItemRules,
         { workItemType: rule.workItemType, rules: [{ ...rule, id: uuidV4() }] }
       ]
     };
@@ -89,6 +93,29 @@ class RuleService {
     return {
       success: true,
       data: created
+    };
+  }
+
+  private async updateOrAddRule(
+    rootDoc: ProjectConfigurationDocument,
+    wiIndex: number,
+    rule: Rule
+  ) {
+    const ruleDocument = rootDoc.workItemRules[wiIndex];
+    const ruleIndex = ruleDocument.rules.findIndex(x => x.id === rule?.id);
+    if (ruleIndex >= 0) {
+      ruleDocument.rules[ruleIndex] = rule;
+    } else {
+      ruleDocument.rules = [...ruleDocument.rules, rule];
+    }
+    const nd = { ...rootDoc };
+    nd.workItemRules[wiIndex] = ruleDocument;
+    const updatedDocument = await this._dataStore.setData(nd);
+
+    this._data = updatedDocument;
+    return {
+      success: true,
+      data: updatedDocument
     };
   }
 }
